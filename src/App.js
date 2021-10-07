99e908902e5c60946a0db039abce4277a25b7ab2
Jessica Wray <jessica.wray@gmail.com>
2021-10-06 18:13:47 -0700

    Rolling works with dynamically created  buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import DiceBtn from './DiceBtn';
+
+function randomNumber( roll = 20 ) {
+  return Math.floor( Math.random() * roll ) + 1
+}
+
+const numBtns = 5;
 
 function App() {
+  const initialValue = Array.from(Array( numBtns ));
+  const [ result, setResult ] = useState( initialValue );
+
+  console.log( 'State is: ', result );
+  
+  function onClick(i) {
+    const [ ...oldArray ] = result;
+    console.log( oldArray );
+    const newNumber = randomNumber();
+    console.log( newNumber  );
+    oldArray.splice( i, 1, newNumber );
+    console.log( oldArray );
+    setResult( oldArray );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Your result is: {result.map( (roll, i) => {
+            return <div key={i}>{roll ? `${roll}` : 'no roll yet'}</div>;
+        } )}
+        {result.map( ( num, index ) => {
+          return (
+            <DiceBtn onClick={ () => onClick( index ) }>
+              Button {index + 1}
+            </DiceBtn>
+          )
+        })}
+
+        <br/>
+        
+        <button onClick={() => setResult([ null, null, null])}>CLEAR</button>
+
       </header>
     </div>
   );
